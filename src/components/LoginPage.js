e28805436ea25f60b29fcbd7213c9b8075f963d9
Aeiou Nicole Garcia <garciaaeiounicole@gmail.com>
2026-01-23 00:31:16 +0800

    medyo okay na yung login page

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,29 +1,37 @@
 import React from 'react';
 import '../styles/LoginPage.css';
-import logo from '../assets/lardizaballogo.svg';
+import logo from '../assets/logo.svg';
 
 export default function LoginPage() {
-  return (
-    <div className="main-container">
-      <div className="card">
-        <img src={logo} alt="Logo" className="card-logo" />
-        <h2 className="card-title">Welcome to Lardizabal Dental Clinic. We missed you!</h2>
-        
-        <input 
-          type="email" 
-          placeholder="email" 
-          className="input" 
-        />
-        <input 
-          type="password" 
-          placeholder="password" 
-          className="input" 
-        />
-        
-        <button className="login-button">
-          LOGIN
-        </button>
-      </div>
-    </div>
-  );
+    return (
+        <div className="main-container">
+        <div className="card">
+            <img src={logo} alt="Logo" className="card-logo" />
+            <div className="label-container">
+                <p className="label">Email</p>
+            </div>
+            <input 
+                type="email" 
+                placeholder="Enter your email" 
+                className="input" 
+            />
+            <div className="label-container">
+                <p className="label">Password</p>
+            </div>
+            <input 
+                type="password" 
+                placeholder="Enter your password" 
+                className="input" 
+            />
+            <button className="login-button">
+                LOGIN
+            </button>
+            <div className="signup-container">
+                <p className="signup-label">
+                    Don't have an account yet? <a href="/signup" className="signup-link">Sign up</a>.
+                </p>
+            </div>
+        </div>
+        </div>
+    );
 }
